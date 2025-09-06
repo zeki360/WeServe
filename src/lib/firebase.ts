debf3b8d23c638740823ec7d0709f2c1ee6f4830
zod <girumzekarias@gmail.com>
2025-09-06 19:23:12 +0300

    images added

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  apiKey: "AIzaSyDYQ18Z56sg7yo5GNpKuk8quWmjtU1oAcQ",
+  authDomain: "we-serve-36073.firebaseapp.com",
+  projectId: "we-serve-36073",
+  storageBucket: "we-serve-36073.appspot.com",
+  messagingSenderId: "364786260503",
+  appId: "1:364786260503:web:243d8ea6223177aeb89a52",
+  databaseURL: "https://we-serve-36073-default-rtdb.firebaseio.com",
+  measurementId: "G-7LGSNNXQC7"
 };
 
 // Initialize Firebase
