d77012948f12842af419d3ef923e81f401cbc38a
Zekarias Girum <Girumzekarias@gmail.com>
2025-10-14 07:35:02 +0000

    change the background color to white, make the menu 3 columns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,7 +93,7 @@ export default function MenuPage() {
         </TabsList>
         {categories.map(category => (
           <TabsContent key={category} value={category} className="mt-6">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               {foodItems
                 .filter(item => item.category === category)
                 .map(item => (
